11de4a31d00a2da4eda9b6871bd1c2dbdbc7104a
andrey <microisoft@gmail.com>
2013-12-01 20:05:54 +0400

    +Fix 0.0.2

--- application/modules/pages/controllers/pages.php
+++ application/modules/pages/controllers/pages.php
@@ -44,6 +44,18 @@ class Pages extends MX_Controller {
         $this->load->view('pages_index', $data);
 
     }
+    public function show()
+    {
+        $news_id = $this->uri->segment(3, 0);
+        if (isset ($news_id) and $news_id != false ){
+
+           echo 'isset :'.$news_id;
+
+        }else {
+            show_error('test', $status_code = 404, 'Ooooops!');
+        }
+    }
+
 }
 
 /* End of file welcome.php */
